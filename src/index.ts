ac850fd35bbd7c22c23efa0b3172f0c00860be1c
zakzakst <e1433104@gmail.com>
2026-05-15 09:57:59 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,38 @@ const showInsertTemplateDialog_ = () => {
   DocumentApp.getUi().showModalDialog(html, "テンプレート選択");
 };
 
+const copyTabContent = (sourceTabId: string) => {
+  const document = DocumentApp.getActiveDocument();
+
+  const sourceBody = document.getTab(sourceTabId).asDocumentTab().getBody();
+
+  const targetBody = document.getActiveTab().asDocumentTab().getBody();
+  const numChildren = sourceBody.getNumChildren();
+
+  for (let i = 0; i < numChildren; i++) {
+    const element = sourceBody.getChild(i).copy();
+
+    const type = element.getType();
+
+    switch (type) {
+      case DocumentApp.ElementType.PARAGRAPH:
+        targetBody.appendParagraph(element);
+        break;
+
+      case DocumentApp.ElementType.LIST_ITEM:
+        targetBody.appendListItem(element);
+        break;
+
+      case DocumentApp.ElementType.TABLE:
+        targetBody.appendTable(element);
+        break;
+    }
+  }
+};
+
 const insertTemplate = (id: string) => {
-  showAlert_(JSON.stringify(id || "ID指定なし"));
+  // showAlert_(JSON.stringify(id || "ID指定なし"));
+  copyTabContent(id);
 };
 
 const onOpen = () => {
