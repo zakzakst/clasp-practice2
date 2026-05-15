63ecf611b8fe59b9e346b6a9a2c2998ddce35d2c
zakzakst <e1433104@gmail.com>
2026-05-15 12:08:08 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { menuInit_ } from "./utils/document/menuInit";
 import { showAlert_ } from "./utils/document/showAlert";
+import { showDialog_ } from "./utils/common/showDialog";
+import { copyTabContent_ } from "./utils/document/copyTabContent";
 
 type TabItem = {
   id: string;
@@ -27,69 +29,16 @@ const getTemplateTabItems = (): TabItem[] | undefined => {
   return childTabItems;
 };
 
-// const insertTemplate_ = (id: string) => {
-//   showAlert_(JSON.stringify(id || "ID指定なし"));
-// };
-
+// TODO: utilsに移動
 const showInsertTemplateDialog_ = () => {
-  const html = HtmlService.createHtmlOutputFromFile("dialog")
-    .setWidth(400)
-    .setHeight(300);
-
-  DocumentApp.getUi().showModalDialog(html, "テンプレート選択");
-};
-
-const copyTabContent = (sourceTabId: string) => {
-  const document = DocumentApp.getActiveDocument();
-
-  const sourceBody = document.getTab(sourceTabId).asDocumentTab().getBody();
-
-  const targetBody = document.getActiveTab().asDocumentTab().getBody();
-  const numChildren = sourceBody.getNumChildren();
-
-  for (let i = 0; i < numChildren; i++) {
-    const element = sourceBody.getChild(i).copy();
-
-    const type = element.getType();
-
-    switch (type) {
-      case DocumentApp.ElementType.PARAGRAPH:
-        targetBody.appendParagraph(element);
-        break;
-
-      case DocumentApp.ElementType.LIST_ITEM:
-        targetBody.appendListItem(element);
-        break;
-
-      case DocumentApp.ElementType.TABLE:
-        targetBody.appendTable(element);
-        break;
-    }
-  }
+  showDialog_("dialog", "テンプレート選択");
 };
 
 const insertTemplate = (id: string) => {
-  // showAlert_(JSON.stringify(id || "ID指定なし"));
-  copyTabContent(id);
+  copyTabContent_(id);
 };
 
 const onOpen = () => {
-  // メニューの関数に直接引数を渡すことはできない
-  // const templateItems = getTemplateTabItems_();
-
-  // if (!templateItems) {
-  //   return;
-  // }
-
-  // const menuItems: MenuItem[] = templateItems.map((item) => {
-  //   return {
-  //     label: `${item.title}挿入`,
-  //     name: `insertTemplate_(${item.id})`,
-  //   };
-  // });
-
-  // menuInit_(menuItems);
-
   menuInit_([
     {
       label: "テンプレート挿入",
